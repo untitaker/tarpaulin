41d3a3b72810d1d9fe40fa878ba771f1220e2cf2
xd009642 <danielmckenna93@gmail.com>
2017-10-17 23:16:50 +0100

    Added bootstrap to html generation.
    Added bootstrap to html generation and started to fill in content.

--- src/report/html_report.rs
+++ src/report/html_report.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+use horrorshow::RenderBox;
 use horrorshow::helper::doctype;
 use horrorshow::prelude::*;
 
@@ -14,9 +15,43 @@ pub fn export(coverage_data: &TraceMap, config: &Config) {
         html {
             head {
                 title: "Tarpaulin Coverage Report";
+                p {
+                    :Raw(r#"<!-- Latest compiled and minified CSS -->
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+                    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+                    crossorigin="anonymous">
+
+                    <!-- Optional theme -->
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
+                    integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" 
+                    crossorigin="anonymous">
+
+                    <!-- Latest compiled and minified JavaScript -->
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+                    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+                    crossorigin="anonymous"></script>"#)
+                }
             }
             body {
-                
+                div(class="container") {
+                    div(class="page-header") {
+                        h1 {
+                            :"Tarpaulin Coverage Report"
+                        }
+                    }
+                    h3 {
+                        :"Run Summary"
+                    }
+                    p {
+                        :format!("Manifest path {}", config.manifest.display()) 
+                    }
+                    h3 {
+                        :"Coverage Results"
+                    }
+                    p {
+                        render_results(coverage_data)
+                    }
+                }
             }
         }
     }.into_string().unwrap();
